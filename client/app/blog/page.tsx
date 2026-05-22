304fbe3b522166b7d4c31b5b28f95140d98f04df
William Jiang <wj2065@nyu.edu>
2026-05-22 16:46:43 -0400

    footer

--- client/app/blog/page.tsx
+++ client/app/blog/page.tsx
@@ -1,23 +1,7 @@
 "use client";
 import { useRouter } from "@/node_modules/next/navigation";
-import Image from "next/image";
-import data from "./data.json";
-import {
-  motion,
-  useScroll,
-  useMotionValueEvent,
-  AnimatePresence,
-} from "motion/react";
-import { useState, useEffect } from "react";
-import { FontAwesomeIcon } from "@/node_modules/@fortawesome/react-fontawesome/dist/index";
-import {
-  faInstagram,
-  faTwitter,
-  faYoutube,
-  faPinterest,
-  faTiktok,
-} from "@fortawesome/free-brands-svg-icons";
 import Header from "../components/header";
+import Footer from "../components/footer";
 
 const Page = () => {
   return (
@@ -25,69 +9,10 @@ const Page = () => {
       <Header />
       {/* scrollable */}
       <div className="flex-1 mt-30 overflow-y-auto space-y-3 bg-zinc-300 scroll-smooth font-sans dark:bg-black">
-        {" "}
         blog format date title blog in markdown
       </div>
       {/* footer  */}
-      <div className="relative flex h-125 pt-30 justify-between w-[90%] mx-auto bg-zinc-500 font-sans dark:bg-black rounded-t-lg text-white">
-        <div className="absolute m-5 top-0 left-0">Logo</div>
-        <div className="flex flex-col pl-10 w-full">
-          {/* follow us */}
-          <div className="font-semibold">Get in touch</div>
-          <div>Email us</div>
-          <div>718-788-2888</div>
-          <div className="font-semibold">Follow Us</div>
-          <div>
-            <FontAwesomeIcon icon={faInstagram} />
-            <FontAwesomeIcon icon={faTwitter} />
-            <FontAwesomeIcon icon={faTiktok} />
-            <FontAwesomeIcon icon={faYoutube} />
-            <FontAwesomeIcon icon={faPinterest} />
-          </div>
-        </div>
-        <div className="flex flex-col w-full">
-          {/* Company */}
-          <div className="font-semibold">Company</div>
-          <div>Catalog</div>
-          <div>Blog</div>
-          <div>Locations</div>
-        </div>
-        <div className="flex flex-col w-full">
-          {/* all product types */}
-          <div className="font-semibold">Products</div>
-          <div>avocado yogurt</div>
-          <div>sous vide chicken breast</div>
-          <div>coconut water</div>
-          <div>sour plum juice</div>
-          <div>dried steak chips</div>
-          <div>honey water</div>
-        </div>
-        <div className="flex flex-col w-full">
-          {/* guides */}
-          <div className="font-semibold">Support</div>
-          <div>Contact</div>
-          <div>Guides</div>
-          <div>Terms and conditions</div>
-          <div>Refund Policy</div>
-          <div>Warranty</div>
-          <div>FAQ</div>
-        </div>
-        <div className="flex flex-col w-full">
-          {/* guides */}
-          <div className="font-semibold">Payment Methods</div>
-          <div>VISA</div>
-          <div>Mastercard</div>
-          <div>Amex</div>
-          <div>Apple Pay</div>
-          <div>Cash App</div>
-          <div>Zelle</div>
-        </div>
-        {/* <div className="flex flex-col">warranty and policies</div>{" "}
-          <div className="flex flex-col">payment methods</div> */}
-        <div className="absolute m-5 bottom-0 flex">
-          Copywright 2026 @ Brand.com
-        </div>
-      </div>
+     <Footer />
     </div>
   );
 };
