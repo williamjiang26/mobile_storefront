6bc7941bf3aa9d16031be91e2fa50825116c58aa
William Jiang <wj2065@nyu.edu>
2026-06-14 12:47:56 -0400

    new

---
+++ client/app/landing/docs/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <div>
+      {/* storefront */}
+      {/*  a mobile site that has xyz benefits  */}
+      {/* preview click for live demo */}
+    </div>
+  );
+};
+
+export default Page;
