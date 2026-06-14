f3419414d5f026b80d2a6f65bfefd035140a8bf6
William Jiang <wj2065@nyu.edu>
2026-06-14 14:56:08 -0400

    new

--- client/proxy.ts
+++ client/proxy.ts
@@ -2,7 +2,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/(.*)"]);
 
-const isPrivateRoute = createRouteMatcher(["/customer","/checkout"])
+const isPrivateRoute = createRouteMatcher(["/customer","/checkout/(.*)"])
 export default clerkMiddleware(async (auth, request) => {
   if (!isPublicRoute(request) || isPrivateRoute(request)) {
     await auth.protect();
