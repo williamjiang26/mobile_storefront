a5d8eb0c0fa6ae66c17146e374bfd43f8923d76d
William Jiang <wj2065@nyu.edu>
2026-06-23 13:32:50 -0400

    new

--- client/app/components/nav.tsx
+++ client/app/components/nav.tsx
@@ -8,7 +8,7 @@ const NavPage = () => {
     <div className="flex flex-col w-full border rounded-t-lg p-7 sm:p-10">
       {/* <div className="">bottom nav</div> */}
       <div className="flex justify-between w-[80%] mx-auto text-lg">
-        <div onClick={() => router.push("/explore")}>explore</div>
+        {/* <div onClick={() => router.push("/explore")}>explore</div> */}
         <div onClick={() => router.push("/catalog")}>catalog</div>
         <div onClick={() => router.push("/bag")}>bag</div>
       </div>
