a7bdff9b59f2ebc12f29afbf56f9b25fa5b2dd55
William Jiang <wj2065@nyu.edu>
2026-06-26 12:46:32 -0400

    new

--- client/app/components/nav.tsx
+++ client/app/components/nav.tsx
@@ -1,18 +1,25 @@
 import { useRouter } from "@/node_modules/next/navigation";
-
-
+import { CircleStar, ReceiptText } from "lucide-react";
 
 const NavPage = () => {
   const router = useRouter();
   return (
-    <div className="flex flex-col w-full border rounded-t-lg p-3 sm:p-7">
-      {/* <div className="">bottom nav</div> */}
-      <div className="flex justify-between w-[80%] mx-auto text-lg">
+    <nav className="w-full bg-white border-t border-gray-200 pb-safe shadow-lg">
+      <div className="w-[50%] mx-auto flex justify-between items-center py-3">
+        {/* <div className="">bottom nav</div> */}
         <div onClick={() => router.push("/explore")}>explore</div>
         <div onClick={() => router.push("/catalog")}>catalog</div>
-        <div onClick={() => router.push("/bag")}>bag</div>
+        <button
+          className="flex flex-col items-center   "
+          onClick={() => router.push("bag")}
+        >
+          <span>
+            <CircleStar className="w-5 h-5" />
+          </span>
+          <span>bag</span>
+        </button>
       </div>
-    </div>
+    </nav>
   );
 };
 
